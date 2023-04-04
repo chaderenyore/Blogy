24d94fa4d37fff36ae4305123613c5cbe09942d8
Nyore Chadere <chaderenyore480@gmail.com>
2023-04-05 00:35:34 +0100

    Feat - Add Backdrop Functionality

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,12 +2,15 @@
   <div>
     <header class="header">
       <div
-        class="container w-[90%] h-[80px] m-auto flex  justify-between items-center"
+        class="backdrop no-display transition-all duration-250 ease-linear"
+        @click="toggleMenu"></div>
+      <div
+        class="container w-[90%] h-[80px] m-auto flex justify-between items-center"
       >
         <div class="header-logo">
           <img src="~/assets/img/Blogy.svg" alt="Your gateway to Blogging" />
         </div>
-        <div class="mobile-menu">
+        <div class="mobile-menu md:hidden">
           <Icon
             name="ion:md-menu"
             size="2.8rem"
@@ -17,10 +20,10 @@
         </div>
       </div>
       <div
-        class="header-items--mobile fixed top-0 right-0 h-[100vh] w-[50%] bg-[#C4C4C4] flex flex-col items-center justify-center transition-all duration-150 ease-in no-display"
+        class="header-items--mobile fixed top-0 right-0 h-[100vh] w-[50%] bg-[#C4C4C4] flex flex-col items-center justify-center transition-all duration-150 ease-in no-display md:hidden"
       >
         <Icon
-          class="absolute top-6 right-8  cursor-pointer"
+          class="absolute top-6 right-8 cursor-pointer"
           name="ion:close-round"
           size="2.3rem"
           @click="toggleMenu"
@@ -38,10 +41,13 @@
 <script setup>
   const toggleMenu = () => {
     const menu = document.querySelector('.header-items--mobile');
+    const backdrop = document.querySelector('.backdrop');
     if (menu.classList.contains('no-display')) {
       menu.classList.remove('no-display');
+      backdrop.classList.remove('no-display');
     } else {
       menu.classList.add('no-display');
+      backdrop.classList.add('no-display');
     }
   };
 </script>
@@ -50,6 +56,15 @@
   html,
   body {
     margin: 0;
+  }
+
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.1);
   }
 
   .nav-link {
